a5ffa471a0831ef60119896b7efb433bfd6727c3
wsgbxx <2036439013@qq.com>
2025-10-18 16:27:00 +0800

    初始提交：数据结构学习项目重新开始

---
+++ src/linear_structures/singly_linked_list.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    // 设置控制台为 UTF-8 编码
+    SetConsoleOutputCP(65001);
+    
+    cout << "Hello, Linked List!" << endl;
+    cout << "程序运行成功！" << endl;
+    return 0;
+}
